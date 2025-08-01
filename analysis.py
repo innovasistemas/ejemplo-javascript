57e035b32a34724f4a203be76d39a1f8a122b39e
SALAS DRAI <innovasistemas@gmail.com>
2025-08-01 16:30:33 -0500

    Pruebas Vue 5.

--- analysis.py
+++ analysis.py
@@ -9,19 +9,26 @@ records = []
 
 for currency, changes in data.items():
     for change in changes:
-        change["currency"] = currency
+        change['Moneda'] = currency
         records.append(change)
 
+# Muestra todos los resultados
+# pd.set_option('display.max_rows', None) 
+
 df = pd.DataFrame(records)
+print('-' * 100)
 df.info()
+print('-' * 100)
+# print('\n')
 print(df.head)
+print('-' * 100)
 print(df.tail)
 
-df['Fecha'] = pd.to_datetime(df['Fecha'], dayfirst=True, errors='coerce')
-df.info()
+# df['Fecha'] = pd.to_datetime(df['Fecha'], dayfirst=True, errors='coerce')
+# df.info()
 
-df.dropna(subset=['Fecha', 'Cambio'], inplace=True)
-print(df.head())
+# df.dropna(subset=['Fecha', 'Cambio'], inplace=True)
+# print(df.head())
 
-df_currency = df.pivot_table(index='Fecha', columns='currency', values='Cambio')
-df_currency.head()
+# df_currency = df.pivot_table(index='Fecha', columns='currency', values='Cambio')
+# df_currency.head()
--- js/newton-binomial.js
+++ js/newton-binomial.js
@@ -1,11 +1,16 @@
 const txtTerm1 = document.querySelector('#txt-a');
 const txtTerm2 = document.querySelector('#txt-b');
 const txtN = document.querySelector('#txt-n');
+const btnDev = document.querySelector('#app-binomial');
 const elements = document.querySelectorAll('.disabled')
 let imageSource = [];
 let TOTAL_IMAGES = 5;
+
 loadImages();
 
+btnDev.addEventListener('click', (e) => {
+    alert(0)
+});
 
 elements.forEach(element => {
     element.addEventListener('focusout', (e) => {
@@ -49,9 +54,9 @@ function disableButton()
     if (txtTerm1.value.trim() !== '') {
         if (txtTerm2.value.trim() !== '') {
             if (txtN.value.trim() !== '') {
+                appBinomial.removeAttribute('disabled')
                 appBinomial.isDisabled = false;
             }
         } 
     } 
-
 }
