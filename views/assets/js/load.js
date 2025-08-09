262b19d073dcc45a83d8596f8fec158678827330
Jaime Montoya <innovasistemas@gmail.com>
2025-08-08 19:02:00 -0500

    Refactorización y cambios en frontend

--- views/assets/js/load.js
+++ views/assets/js/load.js
@@ -1,13 +1,9 @@
-let appTitle = new Vue({
-    el: '#app-title',
-    data: {
-        message: "Vistas con Vue JS"
-    }
-});
-
 const TOTAL_IMAGES = 10;
 let imageSource = [];
+
 loadImages();
+
+
 let appList = new Vue({
     el: '#app-list',
     data: {
@@ -15,9 +11,19 @@ let appList = new Vue({
     }
 });
 
+
+let appTitle = new Vue({
+    el: '#app-title',
+    data: {
+        mainTitle: "Ciencia y Tecnología"
+    }
+});
+
+
 function loadImages()
 {
     for (let i = 1; i <= TOTAL_IMAGES; i++) {
-        imageSource.push({src: `images/img${i}.jpg`, alt: `images/img${i}.jpg`});
+        imageSource.push({src: `assets/images/img${i}.jpg`, alt: `images/img${i}.jpg`});
     }
 }
+
---
+++ views/assets/js/router.js
@@ -0,0 +1,1 @@
+const app = vue.module('')
